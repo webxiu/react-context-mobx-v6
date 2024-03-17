73e8d2932a9365bff0622e4753bf163c99ff307b
Hailen <759430324@qq.com>
2024-03-17 11:06:30 +0800

    fix: 修改严格模式下执行2次问题

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import App from "~/src/App.tsx";
 import "@/index.css";
 import RootStoreProvider from "@/stores/index.tsx";
 
+// 严格模式不能自动检测到你的副作用，但它可以帮助你发现它们，使它们更具确定性。通过故意重复调用以下函数来实现的该操作。
+// 注意：这仅适用于开发模式。生产模式下生命周期不会被调用两次。
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RootStoreProvider>
-      <App />
-    </RootStoreProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RootStoreProvider>
+    <App />
+  </RootStoreProvider>
+  // </React.StrictMode>
 );
